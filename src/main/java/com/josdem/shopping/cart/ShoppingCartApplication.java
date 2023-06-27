1d2b18113225975ecf3a4c4558557bad17332c86
josdem <joseluis.delacruz@gmail.com>
2023-06-27 16:13:09 -0400

    Starting new shopping cart service project :)

---
+++ src/main/java/com/josdem/shopping/cart/ShoppingCartApplication.java
@@ -0,0 +1,12 @@
+package com.josdem.shopping.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShoppingCartApplication.class, args);
+  }
+}
---
+++ src/test/java/com/josdem/shopping/cart/ShoppingCartApplicationTest.java
@@ -0,0 +1,11 @@
+package com.josdem.shopping.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTest {
+
+  @Test
+  void contextLoads() {}
+}
