12a76602f2774dafc7014c0d9829423e8fadae53
josdem <joseluis.delacruz@gmail.com>
2023-06-27 17:07:01 -0400

    Adding application context test

--- src/test/java/com/josdem/shopping/cart/ShoppingCartApplicationTest.java
+++ src/test/java/com/josdem/shopping/cart/ShoppingCartApplicationTest.java
@@ -1,11 +1,28 @@
 package com.josdem.shopping.cart;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
 @SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class ShoppingCartApplicationTest {
 
+  private final ApplicationContext applicationContext;
+
   @Test
-  void contextLoads() {}
+  @DisplayName("loading application")
+  void shouldLoadApplication(TestInfo testInfo) {
+    log.info("Running: {}", testInfo.getDisplayName());
+    ShoppingCartApplication.main(new String[] {});
+    assertNotNull(applicationContext, "should have spring application context");
+  }
 }
