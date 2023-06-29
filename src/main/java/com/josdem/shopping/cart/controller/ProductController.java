c9b2e6330012f9556333e0bc13effd270da47431
josdem <joseluis.delacruz@gmail.com>
2023-06-29 17:24:22 -0400

    #1 Adding get products by id endpoint

--- src/main/java/com/josdem/shopping/cart/controller/ProductController.java
+++ src/main/java/com/josdem/shopping/cart/controller/ProductController.java
@@ -3,21 +3,27 @@ package com.josdem.shopping.cart.controller;
 import com.josdem.shopping.cart.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-  @GetMapping("/")
-  public Flux<Product> getProducts() {
-    return Flux.fromIterable(
-        Arrays.asList(
-            new Product("100", "Nike Air Max", new BigDecimal(1259.00)),
-            new Product("101", "Adidas Air Max", new BigDecimal(1260.00))));
-  }
+    private Map<String, Product> products = Map.of("100", new Product("100", "Nike Air Max", new BigDecimal(1259.00)), "101", new Product("101", "Adidas Air Max", new BigDecimal(1260.00)));
+
+    @GetMapping("/")
+    public Flux<Product> getProducts() {
+        return Flux.fromIterable(products.values());
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Product> getProductById(@RequestParam String id) {
+        return Mono.just(products.get(id));
+    }
 }
