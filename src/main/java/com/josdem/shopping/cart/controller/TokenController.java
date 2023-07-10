6aacbf8f8c603139c0e37dfc2fef70d1bd96ae2d
josdem <joseluis.delacruz@gmail.com>
2023-07-10 16:43:21 -0400

    Adding request mapping annotation

--- src/main/java/com/josdem/shopping/cart/controller/TokenController.java
+++ src/main/java/com/josdem/shopping/cart/controller/TokenController.java
@@ -1,15 +1,17 @@
 package com.josdem.shopping.cart.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/login")
 public class TokenController {
 
-    @PostMapping("/login")
+    @PostMapping("/")
     public Mono<String> generateToken() {
         return Mono.just(UUID.randomUUID().toString());
     }
