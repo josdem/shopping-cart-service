ad39476e1db39257ad51f42e92dcb2f292c737f9
josdem <joseluis.delacruz@gmail.com>
2023-07-10 16:47:50 -0400

    Changing request to get mapping

--- src/main/java/com/josdem/shopping/cart/controller/TokenController.java
+++ src/main/java/com/josdem/shopping/cart/controller/TokenController.java
@@ -1,17 +1,15 @@
 package com.josdem.shopping.cart.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/login")
 public class TokenController {
 
-    @PostMapping("/")
+    @GetMapping("/login")
     public Mono<String> generateToken() {
         return Mono.just(UUID.randomUUID().toString());
     }
