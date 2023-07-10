749b6d3b19f5aa8e804c49184a5ad6094c23a5be
josdem <joseluis.delacruz@gmail.com>
2023-07-10 17:11:36 -0400

    Adding auth response

--- src/main/java/com/josdem/shopping/cart/controller/TokenController.java
+++ src/main/java/com/josdem/shopping/cart/controller/TokenController.java
@@ -1,16 +1,18 @@
 package com.josdem.shopping.cart.controller;
 
+import com.josdem.shopping.cart.model.AuthResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.util.UUID;
-
+@Slf4j
 @RestController
 public class TokenController {
 
     @GetMapping("/login")
-    public Mono<String> generateToken() {
-        return Mono.just(UUID.randomUUID().toString());
+    public Mono<AuthResponse> generateToken() {
+        log.info("Generating token");
+        return Mono.just(new AuthResponse());
     }
 }
