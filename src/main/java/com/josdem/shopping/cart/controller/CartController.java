99df47c3d8d19391627c4b766adc47bcf414688a
josdem <joseluis.delacruz@gmail.com>
2023-07-16 07:52:21 -0400

    Removing items from cart

--- src/main/java/com/josdem/shopping/cart/controller/CartController.java
+++ src/main/java/com/josdem/shopping/cart/controller/CartController.java
@@ -4,7 +4,6 @@ import com.josdem.shopping.cart.config.ApplicationState;
 import com.josdem.shopping.cart.model.Authorization;
 import com.josdem.shopping.cart.model.Product;
 import com.josdem.shopping.cart.service.TokenService;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -26,43 +25,36 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CartController {
 
-    private final TokenService tokenService;
-    private final ApplicationState applicationState;
+  private final TokenService tokenService;
+  private final ApplicationState applicationState;
 
-    private final Map<String, Product> products = new HashMap<>();
+  private final Map<String, Product> products = new HashMap<>();
 
-    @PostConstruct
-    void setup() {
-        log.info("Setting up cart");
-        products.put("100", applicationState.getProducts().get("100"));
-        products.put("101", applicationState.getProducts().get("101"));
+  @PostMapping("/")
+  public Flux<Product> getCart(@RequestBody Authorization authorization) {
+    log.info("Calling cart");
+    if (!tokenService.isValid(authorization.getToken())) {
+      return Flux.empty();
     }
+    return Flux.fromIterable(products.values());
+  }
 
-    @PostMapping("/")
-    public Flux<Product> getCart(@RequestBody Authorization authorization) {
-        log.info("Calling cart");
-        if (!tokenService.isValid(authorization.getToken())) {
-            return Flux.empty();
-        }
-        return Flux.fromIterable(products.values());
+  @PostMapping("/{sku}")
+  public Mono<HttpStatus> addProductById(
+      @PathVariable String sku, @RequestBody Authorization authorization) {
+    log.info("Adding to the cart");
+    if (!tokenService.isValid(authorization.getToken())) {
+      return Mono.just(HttpStatus.UNAUTHORIZED);
     }
+    return Mono.just(applicationState.getProducts().get(sku))
+        .doOnNext(product -> products.put(product.getSku(), product))
+        .map(product -> HttpStatus.OK);
+  }
 
-    @PostMapping("/{sku}")
-    public Mono<HttpStatus> addProductById(@PathVariable String sku, @RequestBody Authorization authorization) {
-        log.info("Adding to the cart");
-        if (!tokenService.isValid(authorization.getToken())) {
-            return Mono.just(HttpStatus.UNAUTHORIZED);
-        }
-        return Mono.just(applicationState.getProducts().get(sku))
-                .doOnNext(product -> products.put(product.getSku(), product))
-                .map(product -> HttpStatus.OK);
-    }
-
-    @DeleteMapping("/")
-    public Mono<HttpStatus> clearProducts() {
-        log.info("Calling clear cart");
-        products.clear();
-        return Mono.just(HttpStatus.OK);
-    }
-
+  @DeleteMapping("/")
+  public Mono<HttpStatus> clearProducts() {
+    log.info("Calling clear cart");
+    products.clear();
+    return Mono.just(HttpStatus.OK);
+  }
 }
