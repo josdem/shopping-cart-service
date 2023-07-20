ee7a5c4acf6621eaa72bd4de5092eabe3e4eb3c7
josdem <joseluis.delacruz@gmail.com>
2023-07-19 20:24:38 -0400

    Formatting code

--- src/main/java/com/josdem/shopping/cart/controller/TokenController.java
+++ src/main/java/com/josdem/shopping/cart/controller/TokenController.java
@@ -19,21 +19,22 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class TokenController {
 
-    private final CredentialsProperties credentialsProperties;
-    private final ApplicationProperties applicationProperties;
+  private final CredentialsProperties credentialsProperties;
+  private final ApplicationProperties applicationProperties;
 
-    @PostMapping(value = "/login")
-    public Mono<AuthResponse> generateToken(@RequestBody AuthRequest authRequest) {
-        log.info("Generating token from data: {}", authRequest.toString());
-        if (authRequest.getUsername().equals(credentialsProperties.getUsername()) && authRequest.getPassword().equals(credentialsProperties.getPassword())) {
-            return Mono.just(new AuthResponse(applicationProperties.getToken()));
-        } else {
-            throw new RuntimeException("Invalid credentials");
-        }
+  @PostMapping(value = "/login")
+  public Mono<AuthResponse> generateToken(@RequestBody AuthRequest authRequest) {
+    log.info("Generating token from data: {}", authRequest.toString());
+    if (authRequest.getUsername().equals(credentialsProperties.getUsername())
+        && authRequest.getPassword().equals(credentialsProperties.getPassword())) {
+      return Mono.just(new AuthResponse(applicationProperties.getToken()));
+    } else {
+      throw new RuntimeException("Invalid credentials");
     }
+  }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleException(Exception exception) {
-        return new ResponseEntity<String>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
-    }
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<String> handleException(Exception exception) {
+    return new ResponseEntity<String>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
+  }
 }
