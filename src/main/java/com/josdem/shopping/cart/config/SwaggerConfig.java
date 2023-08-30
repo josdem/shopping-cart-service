1cacf55dc4edee52a14c5531914495d279e63939
josdem <joseluis.delacruz@gmail.com>
2023-08-30 17:33:50 -0400

    #13 Adding Swagger config

---
+++ src/main/java/com/josdem/shopping/cart/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.josdem.shopping.cart.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+
+  private String swaggerServer;
+
+  public SwaggerConfig(@Value("${swagger.server}") String swaggerServer) {
+    this.swaggerServer = swaggerServer;
+  }
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI().servers(Arrays.asList(new Server().url(swaggerServer)));
+  }
+}
