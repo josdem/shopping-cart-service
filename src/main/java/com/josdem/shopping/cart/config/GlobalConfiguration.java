7e355495a7cc8bf18d4a1f29435cfa9c20971292
josdem <joseluis.delacruz@gmail.com>
2023-10-07 15:58:04 -0400

    #15 Adding cors configuration

---
+++ src/main/java/com/josdem/shopping/cart/config/GlobalConfiguration.java
@@ -0,0 +1,16 @@
+package com.josdem.shopping.cart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class GlobalConfiguration implements WebMvcConfigurer {
+
+  public void addCorsMappings(CorsRegistry corsRegistry) {
+    corsRegistry.addMapping("/**").allowedOrigins("*");
+  }
+
+}
