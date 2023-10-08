c209d9435857135d9d90ac14d4c5fc177a34dfec
josdem <joseluis.delacruz@gmail.com>
2023-10-07 20:33:25 -0400

    #8 Adding secutity dependency and security configuration

---
+++ src/main/java/com/josdem/shopping/cart/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.josdem.shopping.cart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+            (authorize) ->
+                authorize
+                    .requestMatchers(HttpMethod.GET, "/products/**")
+                    .hasAnyAuthority("SCOPE_read")
+                    .anyRequest()
+                    .authenticated())
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    return http.build();
+  }
+}
