f70349778debfc8e430c0afaa4aff700c05c86e4
josdem <joseluis.delacruz@gmail.com>
2023-10-11 17:38:29 -0400

    #18 Adding test case to validate success response on Swagger

---
+++ src/test/java/com/josdem/shopping/cart/config/SwaggerTest.java
@@ -0,0 +1,30 @@
+package com.josdem.shopping.cart.config;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class SwaggerTest {
+
+  private final MockMvc mockMvc;
+
+  @Test
+  @DisplayName("it gets Swagger UI")
+  void shouldGetSwaggerUI(TestInfo testInfo) throws Exception {
+    log.info("Running: {}", testInfo.getDisplayName());
+    mockMvc.perform(get("/swagger-ui.html")).andExpect(status().isOk());
+  }
+}
