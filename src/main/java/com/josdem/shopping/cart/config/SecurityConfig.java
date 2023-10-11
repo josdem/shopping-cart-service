92fadaae9363cc2bea433b50e7ffdaafb9fd9ee4
zhangwt <im.zhangwt@gmail.com>
2023-10-11 15:35:14 +0800

    fix: add swagger whitelist and modify the swaggerServer port
    fixed #18

--- src/main/java/com/josdem/shopping/cart/config/SecurityConfig.java
+++ src/main/java/com/josdem/shopping/cart/config/SecurityConfig.java
@@ -1,19 +1,28 @@
 package com.josdem.shopping.cart.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+
+  private final String[] WHITE_PATH = {
+          "/swagger-ui/**",
+          "/v3/api-docs/**"
+  };
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(
             (authorize) ->
                 authorize
+                    .requestMatchers(WHITE_PATH).permitAll()
                     .requestMatchers("/cart/**", "/login/**", "/products/**")
                     .hasAnyAuthority("SCOPE_write")
                     .anyRequest()
